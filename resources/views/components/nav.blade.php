db8ca21527305994bc00a7d525daa586bf2fa546
Primayunita <primayunitaagustine28@gmail.com>
2026-09-11 12:06:43 +0700

    Rapikan navigasi dan tandai link aktif

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,11 @@
 <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
     <span class="font-semibold">Simple POS</span>
-        <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
-        <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+        <a href="{{ route('pos.create') }}"
+        class="{{ request()->routeIs('pos.create') ? 'text-blue-400 font-semibold' : 'hover:underline' }}">
+            Kasir
+        </a>
+        <a href="{{ route('transactions.index') }}"
+        class="{{ request()->routeIs('transactions.index') ? 'text-blue-400 font-semibold' : 'hover:underline' }}">
+            Transaksi
+        </a>
 </nav>
