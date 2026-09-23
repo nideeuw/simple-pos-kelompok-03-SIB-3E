76c0c28b38583f0cd6d400e5c2f853b6c5c9a06d
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2026-09-24 05:12:50 +0700

    perbaiki n+1 dengan eager loading

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -24,7 +24,7 @@ class TransactionController extends Controller
 
     public function index()
     {
-        $transactions = Transaction::latest()->paginate(15);
+        $transactions = Transaction::with('details.product')->latest()->paginate(15);
         return view('transactions.index', compact('transactions'));
     }
 
