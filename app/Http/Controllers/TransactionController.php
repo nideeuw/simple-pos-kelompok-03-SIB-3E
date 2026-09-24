dd287c808475cee27851cb727605dcd169dda4ff
Primayunita <primayunitaagustine28@gmail.com>
2026-09-24 12:38:27 +0700

    tampilkan nama kasir pada transaksi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -24,7 +24,7 @@ class TransactionController extends Controller
 
     public function index()
     {
-        $transactions = Transaction::with('details.product')->latest()->paginate(15);
+        $transactions = Transaction::with(['details.product', 'user'])->latest()->paginate(15);
         return view('transactions.index', compact('transactions'));
     }
 
